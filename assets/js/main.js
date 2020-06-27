b9f2b1d032a73088928f7acab00d6f1c44807472
ericapsa <ericaps@umich.edu>
2020-06-27 02:06:33 -0400

    Reformatted items, wrote some descriptions, now needs functionality! + pictures

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,4 +156,23 @@
 			}
 		});
 
+		var name = "Curious student, dreamer, tinkerer"
+		var i = 0;
+
+		// Function will type the selected text
+		function typeName() {
+		  if (i<name.length){
+		    // steals html item from my html thru ID
+		    var nameHeader = document.getElementById("NavBarTitle");
+		    // prints out to the console
+		    console.log(nameHeader);
+		    nameHeader.innerHTML = nameHeader.innerHTML + name.charAt(i);
+		    i = i +1;
+		    // How long the type function will run
+		    setTimeout(typeName,100);
+		  }
+		}
+
+		window.onload = typeName;
+
 })(jQuery);
